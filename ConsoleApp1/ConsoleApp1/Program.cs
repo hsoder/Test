f45311e0fb19b82c051cb038a8396d1597ca89fe
Henrik Söder <Henrik.Soder@gymnasiumskovde.se>
2018-08-29 09:08:15 +0200

    Added "math"

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.Write("Mata in ett tal: ");
+            int taL = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Summan är " + (taL + taL));
+
             Console.WriteLine("TEST!COmmit 7777");//Output text in console
             for (int i = 0; i < 10; i++)//Repeats 10 times
             {
